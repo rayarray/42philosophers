e20746c23850c8132adf743ffad125048b7a4aff
Riku Leskinen <rleskine@c1r6p3.hive.fi>
2023-08-26 14:14:21 +0300

    yes lets work more on the script instead of the program

--- getdatarace.sh
+++ getdatarace.sh
@@ -1,21 +1,24 @@
 #/bin/sh
-if [[ -f ./errorlog ]]; then rm ./errorlog; fi
+#if [[ -f ./errorlog ]]; then rm ./errorlog; fi
+TESTDATE="$(date)"
+mkdir "$TESTDATE"
 FILESIZE=0
 let loops=0
 while [ $FILESIZE -eq 0 ]
 do
-	$(./philo 199 195 60 60 > /dev/null 2> ./errorlog)
+	$(./philo 198 195 60 60 > /dev/null 2> "./$TESTDATE/errorlog")
 #	FILESIZE=$(stat -c%s "./errorlog")
-	FILESIZE=$(stat -f%z "./errorlog")
+	FILESIZE=$(stat -f%z "./$TESTDATE/errorlog")
 	let loops++
-#	printf $FILESIZE
+	printf $FILESIZE
 	let divten=loops%10
 	if [ $divten -eq 0 ]; then 
-		printf " $loops " > ./loops
-		date > ./lastrun
+		printf " $loops " 
+		printf "$loops"  > "./$TESTDATE/loops"
+		date > "./$TESTDATE/lastrun"
 	fi
-#	let divten=loops%50
-#	if [[ $divten -eq 0 ]]; then printf "\n"; fi
+	let divten=loops%50
+	if [[ $divten -eq 0 ]]; then printf "\n"; fi
 done
 echo
 echo $loops
